1caa4dda5050af7c559e27b27197ad949b9d0c20
Olena Savelieva <savelieva372@gmail.com>
2021-07-20 13:07:07 +0200

    add style for form

--- planner-ui/src/app/utils/data.utils.ts
+++ planner-ui/src/app/utils/data.utils.ts
@@ -1,3 +1,8 @@
+let today = new Date();
+let day = String(today.getDate()).padStart(2,'0');
+let month = String(today.getMonth() + 1).padStart(2, '0');
+let year =  today.getFullYear();
+
 function ToRomanNumber(num:number): string {
     let lookup:any={M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
     let roman:string='';
@@ -11,15 +16,13 @@ function ToRomanNumber(num:number): string {
     return roman;
 }
 export function GetFormatDate():string{
-    let today = new Date();
-    let day = String(today.getDate()).padStart(2,'0');
-    let month = String(today.getMonth() + 1).padStart(2, '0');
-    let year =  today.getFullYear();
     return day+'.'+ToRomanNumber(+month)+'.'+year;
+}
+export function GetDataNow():string{
+    return year+"-"+month+"-"+day;
 }
 export function GetTime(): string{
     let time = new Date();
-   
     let hours = time.getHours().toString();
     let minutes = time.getMinutes().toString();
     let seconds = time.getSeconds().toString();
@@ -36,4 +39,4 @@ export function GetTime(): string{
     seconds = '0' + seconds;
     }
     return hours + ' : ' + minutes + ' : ' + seconds;
-  }
+}
--- planner-ui/src/app/workspace/workspace.component.ts
+++ planner-ui/src/app/workspace/workspace.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {GetFormatDate,GetTime} from '../utils/data.utils';
+import { FormGroup } from '@angular/forms';
+import {GetDataNow, GetFormatDate,GetTime} from '../utils/data.utils';
 @Component({
   selector: 'app-workspace',
   templateUrl: './workspace.component.html',
   styleUrls: ['./workspace.component.scss']
 })
 export class WorkspaceComponent implements OnInit {
+  addEvent:FormGroup;
   clock:string;
   constructor() {
     setInterval(()=>this.Clock(),1000)
@@ -15,6 +17,7 @@ export class WorkspaceComponent implements OnInit {
   ngOnInit(): void {
   }
   initialCount:string=GetFormatDate();
+  public dataNow:string=GetDataNow();
   Clock(): void {
     this.clock=GetTime();
   }
